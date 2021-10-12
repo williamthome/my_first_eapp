b9a1d16f5b3aa249c7f0dbf648c96365c3a72f3e
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-12 03:14:59 -0300

    test(main): gen_id should return the next id

--- main_test.erl
+++ main_test.erl
@@ -1,7 +1,12 @@
 -module(main_test).
 -include_lib("eunit/include/eunit.hrl").
--import(main, [get_clients/0]).
+-import(main, [get_clients/0, gen_id/0, gen_id/1]).
 -include("client_core.hrl").
 
 get_clients_test() ->
   ?assertNot(length(get_clients()) =:= 0).
+
+gen_id_test() ->
+  ?assertEqual(gen_id([]), 0),
+  Clients = [#client{id = 0}],
+  ?assertEqual(gen_id(Clients), 1).
