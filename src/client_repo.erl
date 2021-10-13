2ba191f933e45a53d122fc757d2162fd68636831
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-13 10:50:25 -0300

    feat: inserts a client by user console inputs

--- src/client_repo.erl
+++ src/client_repo.erl
@@ -1,6 +1,8 @@
 -module(client_repo).
--export([all/0, gen_id/0, gen_id/1, delete/2]).
+-export([all/0, gen_id/0, gen_id/1, delete/2, insert/1]).
 -include("client_core.hrl").
+-import(user_input, [read_string/1]).
+-import(utils, [date_from_pt_br/1]).
 
 all() ->
   [
@@ -28,6 +30,20 @@ gen_id([]) ->
 gen_id(Clients) when is_list(Clients) ->
   #client{id = LastId} = lists:last(Clients),
   LastId + 1.
+
+insert(Clients) when is_list(Clients) ->
+  Name = user_input:read_string("Nome"),
+  Birthdate = date_from_pt_br(user_input:read_string("Data de nascimento")),
+  Address = user_input:read_string("Endereço"),
+  Emails = [user_input:read_string("e-Mail")],
+  Client = #client{
+    id = gen_id(Clients),
+    name = Name,
+    birthdate = Birthdate,
+    address = Address,
+    emails = Emails
+  },
+  [Client | Clients].
 
 delete(Clients, Id)
   when is_list(Clients), is_integer(Id), Id >= 0 ->
